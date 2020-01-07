11eac91f1485ad9f22ac3d46567d7dc6703ed29a
Jared364 <jaredand1994@gmail.com>
2020-01-07 06:02:34 +0000

    Added Crud for Game controller, set up new/show pages to pull and display name for basic setup and testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_050956) do
+ActiveRecord::Schema.define(version: 2020_01_07_051445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2020_01_07_050956) do
     t.string "name"
     t.integer "white_player_id"
     t.integer "black_player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.integer "x_position"
+    t.integer "y_position"
+    t.string "piece_type"
+    t.integer "player_id"
+    t.integer "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
