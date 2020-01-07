3f9232a280c4a0af3ac41d7cd85e853dc17b2592
Jared364 <jaredand1994@gmail.com>
2020-01-07 05:22:31 +0000

    Added base models for the games and the pieces on the game

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_050259) do
+ActiveRecord::Schema.define(version: 2020_01_07_050956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.integer "white_player_id"
+    t.integer "black_player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
